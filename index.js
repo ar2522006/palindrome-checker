47ce1d6431ff5b28b0e25b617576b7f296b7ba58
ar2522006 <ar2522006@gmail.com>
2025-05-20 17:59:12 +0500

    Fix some bugs

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ function checkPalindrome(word) {
   for (let index = cleaned.length - 1; index >= 0; index--) {
     reversed += cleaned[index];
   }
+  if (!word.length) {
+    return
+  }
   if (cleaned === reversed) {
-    document.querySelector("#result").innerHTML = `${word} is a Palindrome!`;
+    document.querySelector("#result").innerHTML = `<span class="font-bold text-black">${word}</span> is a Palindrome!`;
+    document.querySelector("#result").style.color = "green"
+    document.getElementById("infoIco").style.display = "none"
   } else {
-    document.querySelector("#result").innerHTML = `${word} is not a Palindrome!`;
+    document.querySelector("#result").innerHTML = `<span class="font-bold text-black">${word}</span> is not a Palindrome!`;
+    document.querySelector("#result").style.color = "red"
+    document.getElementById("infoIco").style.display = "none"
   }
 }
 
